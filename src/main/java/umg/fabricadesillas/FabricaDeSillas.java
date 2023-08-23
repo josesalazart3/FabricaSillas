2a9c1f617f97c0acd8ecc3cec2747231fe8a1473
jsalazart3 <jsalazart3@miumg.edu.gt>
2023-08-22 22:03:48 -0600

    clase silla

---
+++ src/main/java/umg/fabricadesillas/FabricaDeSillas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umg.fabricadesillas;
+
+/**
+ *
+ * @author josesalazar
+ */
+public class FabricaDeSillas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
